d5c7f368eba029831995a4706d67646d86717ddf
pivotal <pivotal@thompson.(none)>
2011-05-17 18:04:06 -0400

    Modified seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@
 
 Events.delete_all
 
-events = Events.create([{:time => Time.new(2011, 6, 4, 12, 00), :name => "Nick and Nick give a wonderful talk", :speaker => "Nick and Nick"},
+events = Events.create([{:time => Time.local(2011, 6, 4, 12, 00), :name => "Nick and Nick give a wonderful talk", :speaker => "Nick and Nick"},
                         {:time => Time.local(2011, 6, 7, 13, 00), :name => "Best talk on rails ever!!!", :speaker => "Chewbacca"},
                         {:time => Time.local(2011, 6, 2, 14, 00), :name => "MMMMM Dr. Pepper", :speaker => "Thor"},
                         {:time => Time.local(2011, 6, 3, 15, 00), :name => "Check out this lightsaber", :speaker => "Luke Skywalker"}])
