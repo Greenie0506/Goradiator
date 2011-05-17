60297e309dc38a9c61ef6a254247bbf7e27a35c6
pivotal <pivotal@thompson.pivotallabs.com>
2011-05-17 12:21:02 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GorucoRadiator::Application.config.secret_token = '5223a6078efc49a3e50b6055a0a8b39af2071b4613986334e19446f1dce4a4fbb37591d522f526bab429c32fbfb45d5e80f40edf99912f126749bd774a005aef'
