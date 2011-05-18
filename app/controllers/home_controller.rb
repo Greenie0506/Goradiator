7f873f6d5f9a2797e72f54c596b9b8924ce7a3e7
pivotal <pivotal@thompson.(none)>
2011-05-18 15:35:51 -0400

    Refactored Twitter API with passing test. Added Generalization config

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,9 @@ class HomeController < ApplicationController
 
   def index
     @event = Events.upcoming
+    search = Twitter::Search.new
+    @handle_tweets = search.from(APP_CONFIG['twitter_handle']).result_type("recent").per_page(5).fetch
+    search.clear
+    @hashtag_tweets = search.hashtag(APP_CONFIG['twitter_hashtag']).result_type("recent").per_page(5).fetch
   end
-
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,5 +1,31 @@
 require 'spec_helper'
 
 describe HomeController do
-
+  context "Events" do
+    before :each do
+      @double_next_event = double()
+      Events.stub(:upcoming) {@double_next_event}
+    end
+    it "should have an @event object that is the next event" do
+      get 'index'
+      assigns(:event).should == @double_next_event
+    end
+  end
+  context "Twitter" do
+    before :each do
+      @double_twitter_search = double()
+      Twitter::Search.stub(:new) { @double_twitter_search }
+      @double_tweets = double("hello")
+      @double_twitter_search.stub_chain(:hashtag, :result_type, :per_page, :fetch).and_return(@double_tweets)
+      @double_twitter_search.stub_chain(:from, :result_type, :per_page, :fetch).and_return(@double_tweets)
+      @double_twitter_search.stub(:clear)
+      get 'index'
+    end
+    it "should get the tweets with the configured hashtag" do
+      assigns(:hashtag_tweets).should == @double_tweets
+    end
+    it "should get the events twitter handle tweets" do
+      assigns(:handle_tweets).should == @double_tweets
+    end
+  end
 end
