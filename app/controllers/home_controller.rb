758633f2f4a8a6eacfda3eaf9570d51c89f18814
pivotal <pivotal@thompson.(none)>
2011-05-19 10:58:42 -0400

    installed instagram api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,5 +6,9 @@ class HomeController < ApplicationController
     @handle_tweets = search.from(APP_CONFIG['twitter_handle']).result_type("recent").per_page(5).fetch
     search.clear
     @hashtag_tweets = search.hashtag(APP_CONFIG['twitter_hashtag']).result_type("recent").per_page(5).fetch
+    instagram_client = HTTPClient.new
+    instagram_json = instagram_client.get_content("https://api.instagram.com/v1/tags/gotham/media/recent?client_id=e274913172cd4e75a81d3981159c6938")
+    resp = JSON.parse(instagram_json)
+    @gram = resp['data'].first(5).collect { |img| img["images"]["standard_resolution"]["url"] }
   end
 end
