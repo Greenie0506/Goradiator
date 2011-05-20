dcebcd17dedd5a796a1fea28b22324822f0786da
pivotal <pivotal@thompson.(none)>
2011-05-20 09:21:45 -0400

    heroku test

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,14 +3,14 @@ $('#twitter').load('/twitter.js');
 $('#instagram').load('/instagram.js');
 $('#nextEvent').load('/events.js');
 });
-setInterval(function() {
-  $('#twitter').load('/twitter.js');
-}, 5000);
+//setInterval(function() {
+  //$('#twitter').load('/twitter.js');
+//}, 20000);
 
-setInterval(function() {
-  $('#instagram').load('/instagram.js');
-}, 1000);
+//setInterval(function() {
+  //$('#instagram').load('/instagram.js');
+//}, 20000);
 
-setInterval(function(){
-  $('#nextEvent').load('/event.js');
-}, 600000);
+//setInterval(function(){
+  //$('#nextEvent').load('/event.js');
+//}, 600000);
