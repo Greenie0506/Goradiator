4d35c8a82519244a8aa53e3b5139cec8920997dd
Nicholas Greenfield & Nicholas Monje <pair+Nickg+monje@pivotallabs.com>
2011-05-20 15:19:49 -0400

    Twitter profile pictures added to twitter api

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,7 +1,7 @@
 class TwitterController < ApplicationController
   def index
     search = Twitter::Search.new
-    @handle_tweets = search.from(APP_CONFIG['twitter_handle']).result_type("recent").per_page(5).fetch
+    @handle_tweets = search.from(APP_CONFIG['twitter_handle']).result_type("recent").per_page(1).fetch
     search.clear
     @hashtag_tweets = search.hashtag(APP_CONFIG['twitter_hashtag']).result_type("recent").per_page(5).fetch
     respond_to do |format|
