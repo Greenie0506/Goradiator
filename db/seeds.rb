a3a61e6cb226bd51a4e58d6e78df839ace00b6ed
Nicholas Greenfield & Nicholas Monje <pair+Nickg+monje@pivotallabs.com>
2011-05-20 11:14:32 -0400

    Made events specifiable in config.yml

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,9 @@
 
 Events.delete_all
 
-events = Events.create([{:time => Time.local(2011, 6, 4, 12, 00), :name => "Nick and Nick give a wonderful talk", :speaker => "Nick and Nick"},
-                        {:time => Time.local(2011, 6, 7, 13, 00), :name => "Best talk on rails ever!!!", :speaker => "Chewbacca"},
-                        {:time => Time.local(2011, 6, 2, 14, 00), :name => "MMMMM Dr. Pepper", :speaker => "Thor"},
-                        {:time => Time.local(2011, 6, 3, 15, 00), :name => "Check out this lightsaber", :speaker => "Luke Skywalker"}])
-
+APP_CONFIG['events'].each do |event|
+  Events.create(:time => Time.parse(event['time']), :name => event['name'], :speaker => event['speaker'])
+end
 
 
 
