04e2ab6597116a5b85bf7477ec285c211a9cf353
nmonje <pair+aberlin+akwiatkowski@redroverhq.com>
2011-05-25 13:33:11 -0400

    Added fuubar gem. Fixed instagram spec

--- spec/controllers/instagram_controller_spec.rb
+++ spec/controllers/instagram_controller_spec.rb
@@ -2,19 +2,26 @@ require 'spec_helper'
 
 describe InstagramController do
   context "loading Instagram images" do
+    render_views
+
     before :each do
       instagram_client = double()
       HTTPClient.stub(:new) {instagram_client}
-      double_instagram_images = "Fake image"
-      instagram_response = double()
-      instagram_client.stub(:get_content).and_return(instagram_response)
-      JSON.stub(:parse).and_return(instagram_response)
-      instagram_response.stub_chain(:[], :first, :collect).and_return(double_instagram_images)
-      get 'index'
+      @instagram_images = ["Fake image"]
+      @instagram_response = double()
+      instagram_client.stub(:get_content).and_return(@instagram_response)
+      JSON.stub(:parse).and_return(@instagram_response)
     end
     
     it "should get the most recent instagram photos with the tag" do
-      assigns(:instagram_images).should == "Fake image"
+      @instagram_response.stub_chain(:[], :first, :collect).and_return(@instagram_images)
+      get :index
+      
+      assigns(:instagram_images).should include("Fake image")
+    end
+
+    it "should show a notice when there are no instagram images" do
+      
     end
   end
 end
