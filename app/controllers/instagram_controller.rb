4bb13cbd01e2c94c4e45852e26f5623c06bbb497
Alex Kwiatkowski & Nicholas Monje <pair+akwiatkowski+monje@pivotallabs.com>
2011-05-26 16:02:49 -0400

    Implemented cache database for api calls.

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -1,12 +1,36 @@
 class InstagramController < ApplicationController
   def index
-    instagram_client = HTTPClient.new
-    instagram_response = instagram_client.get_content("https://api.instagram.com/v1/tags/#{APP_CONFIG['instagram_tag']}/media/recent?client_id=#{APP_CONFIG['instagram_client_id']}")
-    instagram_response = JSON.parse(instagram_response)
-    @instagram_images = instagram_response['data'].first(10).collect { |img| img["images"]["standard_resolution"]["url"] }
+    @instagram_tag = APP_CONFIG['instagram_tag']
+    service_type = "instagram_tag_#{@instagram_tag}"
+    valid_for = 20 #in seconds
+    data = cache_data(service_type, @instagram_tag, valid_for)
+    @instagram_images = JSON.parse(data)['data']
 
     respond_to do |format|
       format.html { render :partial => 'index' }
     end
+  end
+
+  private
+  def instagram_call instagram_tag
+    in_url = "https://api.instagram.com/v1/tags/#{instagram_tag}/media/recent?client_id=#{APP_CONFIG['instagram_client_id']}"
+    http_client = HTTPClient.new
+    return http_client.get_content(in_url)
+  end
+
+  def cache_data service_type, instagram_tag, valid_for
+    cache = ApiCache.find_by_service_type(service_type)
+    if (cache.nil? || cache.is_expired?)
+      if (not cache.nil?)
+        cache.delete
+      end
+      data = instagram_call instagram_tag
+      ApiCache.create(:service_type => service_type,
+                       :valid_for => valid_for,
+                       :data => data)
+    else
+      data = cache.data
+    end
+    return data
   end
 end
