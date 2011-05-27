974565d8fc483d9e50591a8569179ed28c3c13de
Alex Kwiatkowski & Nicholas Monje <pair+akwiatkowski+monje@pivotallabs.com>
2011-05-27 11:57:14 -0400

    take 2

--- db/migrate/20110527154838_change_data_to_text_and_valid_for_to_int.rb
+++ db/migrate/20110527154838_change_data_to_text_and_valid_for_to_int.rb
@@ -1,11 +1,13 @@
 class ChangeDataToTextAndValidForToInt < ActiveRecord::Migration
   def self.up
     change_column :api_caches, :data, :text
-    change_column :api_caches, :valid_for, :integer
+    remove_column :api_caches, :valid_for
+    add_column :api_caches, :valid_for, :integer
   end
 
   def self.down
     change_column :api_caches, :data, :string
-    change_column :api_caches, :valid_for, :time
+    remove_column :api_caches, :valid_for
+    add_column :api_caches, :valid_for, :time
   end
 end
